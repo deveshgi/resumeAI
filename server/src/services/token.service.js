3e2aaae0910208058b330c10ecae4f4ceb05eb04
Devesh-sah <deveshmuz2020@gmail.com>
2026-06-04 00:41:05 +0530

    fix error

--- server/src/services/token.service.js
+++ server/src/services/token.service.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.model.js";
-import { ApiError } from "../../../../utils/ApiError.js";
+import { ApiError } from "../utils/ApiError.js";
 
 export const generateTokens = async (userId) => {
   try {
---
+++ server/src/utils/handler.js
@@ -0,0 +1,9 @@
+const handler = (reqHandler) => {
+  return(req, res, next) => {
+    Promise.resolve(reqHandler(req, res, next))
+    .catch((error) => next(error));
+  }
+};
+
+export { handler };
+
